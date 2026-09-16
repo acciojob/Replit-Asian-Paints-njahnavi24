152f0de54383035b74dd0b68cfd6076fcfd45aa0
njahnavi24 <jahnavinagavalli24@gmail.com>
2026-09-16 12:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ changeBtn.addEventListener("click", () => {
 		item.style.backgroundColor = "transparent"
 	})
 
-	let selectedBlock = document.getElementById(blockId.value)
+	let selectedBlock = document.getElementById('#' + blockId.value)
 
 	if(selectedBlock) selectedBlock.style.backgroundColor = colourId.value
 })
